d00749455aa26c2c49ec7e801d5e4dc6c9e9a5be
Michael <ninjuh1124@gmail.com>
2019-07-30 18:43:40 -0400

    Pooled connection

--- db/helpers.js
+++ db/helpers.js
@@ -1,90 +1,52 @@
-const MongoClient = require('mongodb').MongoClient;
-
-exports.loadThreads = (callback) => {
-	let uri = process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost/fridaydotmoe';
-	MongoClient.connect(uri)
-		.then(db => {
+module.exports = (db) => {
+	return {
+		loadThreads: callback => {
 			db.collection('threads')
 				.find({ kind: 'submission' })
 				.toArray( (err, arr) => {
 					callback(null, arr);
-					db.close();
 				});
-		})
-		.catch(err => {
-			callback(err);
-		});
-}
+		},
 
-// checks for new sticky
-exports.isNewCDF = (obj) => {
-	obj = obj.toJSON();
-	return (
-		obj.author_fullname == 't2_6wrl6'
-		&& obj.title.includes("Casual Discussion Friday")
-	);
-}
+		isNewCDF: obj => {
+			obj = obj.toJSON();
+			return (
+				obj.author_fullname == 't2_6wrl6'
+				&& obj.title.includes("Casual Discussion Friday")
+			);
+		},
 
-// removes unnecessary object fields
-exports.handleThread = (thread) => {
-	obj = thread.toJSON();
-	return {
-		kind:		'submission',
-		_id:		obj.name,
-		id: 		obj.id,
-		permalink: 	'https://redd.it/' + obj.id
-	};
-}
+		handleThread: thread => {
+			obj = thread.toJSON();
+			return {
+				kind:		'submission',
+				_id:		obj.name,
+				id: 		obj.id,
+				permalink: 	'https://redd.it/' + obj.id
+			};
+		},
 
-exports.handleComment = (obj) => {
-	obj = obj.toJSON();
-	return {
-		kind: 		'comment',
-		author: 	obj.author,
-		_id: 		obj.name,
-		id: 		obj.id,
-		created: 	obj.created_utc,
-		permalink: 	'https://reddit.com' + obj.permalink,
-		parentID: 	obj.parent_id,
-		body: 		obj.body
-	};
-}
+		handleComment: comment => {
+			obj = obj.toJSON();
+			return {
+				kind: 		'comment',
+				author: 	obj.author,
+				_id: 		obj.name,
+				id: 		obj.id,
+				created: 	obj.created_utc,
+				permalink: 	'https://reddit.com' + obj.permalink,
+				parentID: 	obj.parent_id,
+				body: 		obj.body
+			};
+		},
 
-// stores obj to appropraite collection
-exports.store = (obj) => {
-	const uri = process.env.MONGO_URI ? process.env.MONGO_URI : "mongodb://localhost/fridaydotmoe";
-	let collection;
-	if (obj.kind === 'comment') collection = 'comments';
-	else if (obj.kind === 'submission') collection = 'threads';
+		store: obj => {
+			let collection;
+			if (obj.kind === 'comment') collection = 'comments';
+			else if (obj.kind === 'submission') collection = 'threads';
 
-	MongoClient.connect(uri)
-		.then(db => {
 			db.collection(collection)
 				.insertOne(obj);
-			db.close();
-		})
-		.catch(error => {
-			console.log(error);
-		});
-};
-
-exports.upsert = (obj) => {
-	const uri = process.env.MONGO_URI ? process.env.MONGO_URI : "mongodb://localhost/fridaydotmoe";
-	let collection;
-	if (obj.kind === 'comment') collection = 'comments';
-	else if (obj.kind === 'submission') collection = 'threads';
-
-	MongoClient.connect(uri)
-		.then(db => {
-			db.collection(collection)
-				.update(
-					{ _id : _obj.id },
-					obj,
-					{ upsert: true }
-				);
-			db.close();
-		})
-		.catch(error => {
-			console.log(error);
-		});
+		}
+	}
 }
--- db/index.js
+++ db/index.js
@@ -1,58 +1,69 @@
 const dotenv = require('dotenv'),
 	snoowrap = require('snoowrap'),
-	snoostorm = require('snoostorm');
-
-const helpers = require('./helpers');
+	snoostorm = require('snoostorm')
+	MongoClient = require('mongodb').MongoClient;
 
 dotenv.load();
 
 const cred = {
-	userAgent: process.env.REDDIT_USER_AGENT,
-	clientId: process.env.REDDIT_CLIENT_ID,
-	clientSecret: process.env.REDDIT_CLIENT_SECRET,
-	username: process.env.REDDIT_USERNAME,
-	password: process.env.REDDIT_PASSWORD
+	userAgent:      process.env.REDDIT_USER_AGENT,
+	clientId:       process.env.REDDIT_CLIENT_ID,
+	clientSecret:   process.env.REDDIT_CLIENT_SECRET,
+	username:       process.env.REDDIT_USERNAME,
+	password:       process.env.REDDIT_PASSWORD
 };
 
 var threads = [];
 
-helpers.loadThreads( (err, arr) => {
-	if (err) {
-		console.log(err);
-		process.exit(1);
-	}
-	threads = arr.map(d => d._id);
-});
-
-const reddit = new snoowrap(cred);
 const listener = new snoostorm(cred);
 
-reddit.config({
-	requestDelay: 5000,
-	debug: true
+MongoClient.connect(
+	(process.env.MONGO_URI ?
+		process.env.MONGO_URI :
+		'mongodb://localhost/fridaydotmoe')
+).then(pool => {
+	const helpers = require('./helpers')(pool);
+
+	/*
+	const reddit = new snoowrap(cred);
+
+	reddit.config({
+		requestDelay: 5000,
+		debug: true
+	});
+	*/
+
+	helpers.loadThreads( (err, arr) => {
+		if (err) {
+			console.log(err);
+			process.exit(1);
+		}
+		threads = arr.map(d => d._id);
+	});
+
+	const commentStream = listener.CommentStream({
+		subreddit: 'anime',
+		results: 100,
+		polltime: 10000
+	});
+
+	const submissionStream = listener.SubmissionStream({
+		subreddit: 'anime',
+		results: 50,
+		polltime: 30000
+	});
+
+	commentStream.on('comment', comment => {
+		if (threads.includes(comment.link_id)) {
+			helpers.store(helpers.handleComment(comment));
+		}
+	});
+	submissionStream.on('submission', thread => {
+		if (helpers.isNewCDF(thread)) {
+			helpers.store(helpers.handleThread(thread));
+			threads.unshift(thread.name);
+			threads.pop();
+		}
+	});
 });
 
-const commentStream = listener.CommentStream({
-	subreddit: 'anime',
-	results: 100,
-	polltime: 10000
-});
-
-const submissionStream = listener.SubmissionStream({
-	subreddit: 'anime',
-	results: 50,
-	polltime: 30000
-});
-
-commentStream.on('comment', comment => {
-	if (threads.includes(comment.link_id)) {
-		helpers.store(helpers.handleComment(comment));
-	}
-});
-submissionStream.on('submission', thread => {
-	if (helpers.isNewCDF(thread)) {
-		helpers.store(helpers.handleThread(thread));
-		threads.unshift(thread.name);
-		threads.pop();
-	}
-});
