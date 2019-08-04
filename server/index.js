326cb76b1409d4a04e7891b6e5afd6f8ad2a1337
Michael <ninjuh1124@gmail.com>
2019-08-04 19:17:43 -0400

    Need to fix CORS

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ MongoClient.connect(
 	app.use( (req, res, next) => {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept');
+		res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
 		next();
 	});
 	app.use(bodyParser.urlencoded({ extended: true }));
