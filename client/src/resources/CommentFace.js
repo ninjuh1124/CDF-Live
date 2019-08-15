ba849c77447216bf060346beecd795628bc2bb76
Michael <ninjuh1124@gmail.com>
2019-08-15 15:10:20 -0400

    fixed 'hover' type comment face behavior

--- client/src/resources/CommentFace.js
+++ client/src/resources/CommentFace.js
@@ -3,26 +3,50 @@ import facecodes from './facecodes';
 
 const CommentFace = (props) => {
 	if (!facecodes[props.code]) return null;
-	console.log(facecodes[props.code].height);
 	let style = {
 		height: facecodes[props.code].height+'px',
 		width: facecodes[props.code].width+'px'
 	}
-
 	return(
 		<span className="comment-face" style={style} title={props.title}>
-			<img
-				src={ '/faces/' +
-					(facecodes[props.code].active ? 
-						'active/' : 
-						'discontinued/'
-					) +
-					facecodes[props.code].filename
-				}
-				alt={props.code}
-				className="face"
-				style={style}
-			/>
+			{ facecodes[props.code].type !== 'hover' ?
+				<img
+					src={ '/faces/' +
+						(facecodes[props.code].active ? 
+							'active/' : 
+							'discontinued/'
+						) +
+						facecodes[props.code].filename
+					}
+					alt={props.code}
+					className="face"
+					style={style}
+				/> :
+				<><img
+					src={ '/faces/' +
+						(facecodes[props.code].active ? 
+							'active/' : 
+							'discontinued/'
+						) +
+						facecodes[props.code].filename2
+					}
+					alt={props.code}
+					className="face face-hover"
+					style={style}
+				/><img
+					src={ '/faces/' +
+						(facecodes[props.code].active ? 
+							'active/' : 
+							'discontinued/'
+						) +
+						facecodes[props.code].filename1
+					}
+					alt={props.code}
+					className="face face-no-hover"
+					style={style}
+				/>
+				</>
+			}
 			<span className="face-text">{props.children}</span>
 		</span>
 	)
--- client/src/resources/renderers.js
+++ client/src/resources/renderers.js
@@ -2,10 +2,16 @@ import React from 'react';
 import CommentFace from './CommentFace';
 import Spoiler from './Spoiler';
 
+const supportsStringRender = parseInt((React.version || '16').slice(0, 2), 10) >= 16
+const createElement = React.createElement
+
 export default {
+	sup: props => {
+		return <sup>{props.children}</sup>
+	},
+
 	link: props => {
 		if (/^#\S+/.test(props.href)) {
-			console.log(props.href);
 			return <CommentFace
 				code={props.href}
 				title={props.title}
