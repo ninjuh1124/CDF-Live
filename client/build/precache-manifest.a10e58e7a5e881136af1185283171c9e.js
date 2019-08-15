a0ab3ac281208f2f4f1763d58104ebd23a10171b
Michael <ninjuh1124@gmail.com>
2019-08-15 15:11:38 -0400

    Updated build

--- client/build/precache-manifest.a10e58e7a5e881136af1185283171c9e.js
+++ client/build/precache-manifest.a10e58e7a5e881136af1185283171c9e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a12066d4c8a5666e87b9dcaba6b8a9ed",
+    "revision": "13ed5c9d706287fc6956bea74cbde573",
     "url": "/index.html"
   },
   {
-    "revision": "45d7c758c6ab3bd1434d",
-    "url": "/static/css/main.290c2c61.chunk.css"
+    "revision": "55f6d4691a8d5f1b1012",
+    "url": "/static/css/main.4a3635ab.chunk.css"
   },
   {
     "revision": "e16f6d16a415ae4e1b24",
     "url": "/static/js/2.21760683.chunk.js"
   },
   {
-    "revision": "45d7c758c6ab3bd1434d",
-    "url": "/static/js/main.5e23f282.chunk.js"
+    "revision": "55f6d4691a8d5f1b1012",
+    "url": "/static/js/main.254a5ec8.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
