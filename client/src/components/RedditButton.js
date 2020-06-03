b2a57727d928b6044c1cb44e1385dcb805f64adf
ninjuh1124 <ninjuh1124@gmail.com>
2020-06-03 15:14:33 -0400

    Muted warning

--- client/src/components/RedditButton.js
+++ client/src/components/RedditButton.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const RedditButton = props => (
-	<a
+	<button
 		className={props.className || 'reddit-button link-primary'}
-		href={props.href || 'javascript:void(0)'}
 		onClick={props.onClick}
 		{...props}
-	>{props.children}</a>
+	>{props.children}</button>
 );
 
 export default RedditButton;
